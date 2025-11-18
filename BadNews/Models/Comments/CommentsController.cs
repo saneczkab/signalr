0344ad6f420d06852a0564a206e5c4b9dea6a165
Daewoow <147472046+Daewoow@users.noreply.github.com>
2025-11-18 15:07:08 +0500

    1.2

---
+++ BadNews/Models/Comments/CommentsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using BadNews.Models.Comments;
+using BadNews.Repositories.Comments;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Controllers
+{
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentsRepository commentsRepository;
+
+        public CommentsController(CommentsRepository commentsRepository)
+        {
+            this.commentsRepository = commentsRepository;
+        }
+
+        [HttpGet("api/news/{newsId}/comments")]
+        public ActionResult<CommentsDto> GetCommentsForNews(Guid newsId)
+        {
+            var comments = commentsRepository.GetComments(newsId)
+                ?.Select(comment => new CommentDto
+                {
+                    User = comment.User,
+                    Value = comment.Value
+                })
+                .ToArray() ?? [];
+
+            var result = new CommentsDto
+            {
+                NewsId = newsId,
+                Comments = comments
+            };
+
+            return Ok(result);
+        }
+    }
+}
